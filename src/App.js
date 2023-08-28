c0ccb5ea758f043a9c917449fef6b8ca1e1bcb6e
Julian Codespoti <julian.codespoti@myob.com>
2023-08-28 16:22:51 +1000

    set up home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const navigateToDashboard = () => {
+    console.log("Navigating to Members Dashboard...");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to GotoGro-MRM</h1>
       </header>
+      <main>
+        <p>
+          Situated in the Hawthorn region, Goto Grocery operates as a member-centric grocery store 
+          facing challenges in meeting its members' expectations and satisfying their diverse grocery needs.
+          We're excited to introduce our new digital Members Record Management System.
+        </p>
+        <button onClick={navigateToDashboard}>Go to Members Dashboard</button>
+      </main>
+      <footer>
+        <p>© 2023 Goto Grocery Inc. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
