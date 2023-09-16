0877188010b2f5ff4f534ef1e76410df736247fe
Marella Morad <marella99mourad@gmail.com>
2023-09-16 21:14:22 +1000

    Link to db

--- src/Supabase/supabaseService.js
+++ src/Supabase/supabaseService.js
@@ -21,3 +21,18 @@ export const signIn = async (email, password) => {
 
     return user;
 };
+
+export const search = async (name) => {
+    try {
+        let { data: Members, error } = await supabase
+            .from('Members')
+            .select('*')
+            .or(`first_name.ilike.%${name}%,last_name.ilike.%${name}%`);
+
+        if (error) throw error;
+
+        return Members;
+    } catch (error) {
+        throw error;
+    }
+}
