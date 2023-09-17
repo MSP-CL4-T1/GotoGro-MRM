81c563c82180e8702278bc643244cca9c864afb4
Marella Morad <marella99mourad@gmail.com>
2023-09-17 23:18:18 +1000

    Connect to db

--- src/Pages/MembersDashboard/MembersDashboard.js
+++ src/Pages/MembersDashboard/MembersDashboard.js
@@ -1,29 +1,50 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { search } from '../../Supabase/supabaseService';
+import { searchMembersByName } from '../../Supabase/supabaseService';
 import './MembersDashboard.css';
 
-
+/**
+ * MembersDashboard component for displaying and searching members.
+ * @returns {JSX.Element} The rendered JSX element.
+ */
 function MembersDashboard() {
     const [members, setMembers] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [showNoMembersFound, setShowNoMembersFound] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
+    /**
+     * Handles the search for members.
+     * @param {Event} e - The event object.
+     */
     const handleSearch = async (e) => {
         e.preventDefault();
 
         try {
-            const searchResults = await search(searchTerm);
+            setIsLoading(true);
+            const searchResults = await searchMembersByName(searchTerm);
             setMembers(searchResults);
 
             // Show "No Members Found" if no results
             setShowNoMembersFound(searchResults.length === 0);
         } catch (error) {
             console.error(error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
+    /**
+     * Saves the selected member to local storage to retrieve it later in Member component
+     * @param member - The selected Member
+     */
+    const saveSelectedMemberToLocalStorage = (member) => {
+        localStorage.setItem('selectedMember', JSON.stringify(member));
+    };
 
+    /**
+     * Clears the search and member list.
+     */
     const handleClear = () => {
         setSearchTerm('');
         setMembers([]);
@@ -32,47 +53,63 @@ function MembersDashboard() {
 
     return (
         <div className='card'>
-            <div className='card-content'>
-                <h2>Members Dashboard</h2>
-                <div>
-                    <input
-                        className='search-input'
-                        type="text"
-                        placeholder="Search members..."
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <div className='btn-container'>
-                        <button className='btn' onClick={handleSearch}>Search</button>
-                        <button className='btn' onClick={handleClear}>Clear</button>
-                        <Link className='link-btn' to="/add-member">Add New Member</Link>
-                    </div>
+            <h2>Members Dashboard</h2>
+            <div>
+                <input
+                    className='search-input'
+                    type="text"
+                    placeholder="Search members..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <div className='btn-container'>
+                    <button className='btn' onClick={handleSearch}>Search</button>
+                    <button className='btn' onClick={handleClear}>Clear</button>
+                    <Link className='link-btn' to="/add-member">Add New Member</Link>
                 </div>
-                {members.length > 0 ? (
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Email</th>
-                                <th>Date Joined</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {members.map((member) => (
-                                <tr key={member.id}>
-                                    <td>{member.firstName}</td>
-                                    <td>{member.lastName}</td>
-                                    <td>{member.email}</td>
-                                    <td>{member.dateJoined}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                ) : showNoMembersFound ? (
-                    <p>No Members Found</p>
-                ) : null}
             </div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : members.length > 0 ? (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Date Joined</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {members.map((member) => (
+                            <tr key={member.member_id}>
+                                <td><Link
+                                    key={member.member_id}
+                                    to={'/member'}
+                                    onClick={() => { saveSelectedMemberToLocalStorage(member); }}
+                                >{member.first_name}</Link></td>
+                                <td><Link
+                                    key={member.member_id}
+                                    to={'/member'}
+                                    onClick={() => { saveSelectedMemberToLocalStorage(member); }}
+                                >{member.last_name}</Link></td>
+                                <td><Link
+                                    key={member.member_id}
+                                    to={'/member'}
+                                    onClick={() => { saveSelectedMemberToLocalStorage(member); }}
+                                >{member.email}</Link></td>
+                                <td><Link
+                                    key={member.member_id}
+                                    to={'/member'}
+                                    onClick={() => { saveSelectedMemberToLocalStorage(member); }}
+                                >{member.date_joined}</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : showNoMembersFound ? (
+                <p>No Members Found</p>
+            ) : null}
         </div>
     );
 }
