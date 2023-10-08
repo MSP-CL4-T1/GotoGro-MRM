8f71e5470e716edf1036498103b81735c5788499
Alexander Kyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2023-10-08 13:08:30 +1100

    Moved order of low stock alerts so that they show first. added condition so that low stock alerts only show if there is items that are low in stock, Added button that allows for low stock alerts to be reordered manually. Adjusted css so that footer does not block charts

--- src/Pages/ProductsDashboard/ProductsDashboard.js
+++ src/Pages/ProductsDashboard/ProductsDashboard.js
@@ -1,146 +1,177 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts, updateProduct } from '../../Supabase/supabaseService';
-import {Bar, Doughnut, Pie} from 'react-chartjs-2';
+import { Bar, Doughnut, Pie } from 'react-chartjs-2';
 import './ProductsDashboard.css';
 import 'chartjs-adapter-date-fns';
-import {CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, Chart, ArcElement} from 'chart.js';
+import {
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Chart,
+  ArcElement,
+} from 'chart.js';
 
-const LOW_STOCK_THRESHOLD = 5; // Define a threshold to determine when stock is "low"
-Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
+const LOW_STOCK_THRESHOLD = 5;
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
 
 const ProductsDashboard = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const getProducts = async () => {
-            const fetchedProducts = await fetchProducts();
-            setProducts(fetchedProducts);
-        };
+  const getProducts = async () => {
+    const fetchedProducts = await fetchProducts();
+    setProducts(fetchedProducts);
+  };
 
-        getProducts();
-    }, []);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    useEffect(() => {
-        products.forEach(product => {
-            if (product.stock_quantity <= LOW_STOCK_THRESHOLD) {
-                reorderStock(product);
-            }
-        });
-    }, [products]);
+  const handleReorderStockClick = async () => {
+    products.forEach((product) => {
+      if (product.stock_quantity <= LOW_STOCK_THRESHOLD) {
+        reorderStock(product);
+      }
+    });
 
-    const reorderStock = (product) => {
-        const reorderedProduct = {
-            ...product,
-            stock_quantity: product.stock_quantity + 10 // Reorder quantity
-        };
-        updateProduct(reorderedProduct);
+    await getProducts();
+  };  
+
+  const reorderStock = (product) => {
+    const reorderedProduct = {
+      ...product,
+      stock_quantity: product.stock_quantity + 10,
     };
+    updateProduct(reorderedProduct);
+  };
 
-    const dataBar = {
-        labels: products.map(p => p.product_name),
-        datasets: [{
-            label: 'Stock Quantity',
-            data: products.map(p => p.stock_quantity),
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
-    };
+  const dataBar = {
+    labels: products.map((p) => p.product_name),
+    datasets: [
+      {
+        label: 'Stock Quantity',
+        data: products.map((p) => p.stock_quantity),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    const dataPie = {
-        labels: products.map(p => p.product_name),
-        datasets: [{
-            data: products.map(p => p.stock_quantity),
-            backgroundColor: products.map(() => '#' + Math.floor(Math.random()*16777215).toString(16))
-        }]
-    };
+  const dataPie = {
+    labels: products.map((p) => p.product_name),
+    datasets: [
+      {
+        data: products.map((p) => p.stock_quantity),
+        backgroundColor: products.map(() => '#' + Math.floor(Math.random() * 16777215).toString(16)),
+      },
+    ],
+  };
 
-    const dataDoughnut = {
-        labels: products.map(p => p.product_name),
-        datasets: [{
-            data: products.map(p => p.price * p.stock_quantity),
-            backgroundColor: products.map(() => '#' + Math.floor(Math.random()*16777215).toString(16))
-        }]
-    };
+  const dataDoughnut = {
+    labels: products.map((p) => p.product_name),
+    datasets: [
+      {
+        data: products.map((p) => p.price * p.stock_quantity),
+        backgroundColor: products.map(() => '#' + Math.floor(Math.random() * 16777215).toString(16)),
+      },
+    ],
+  };
 
-    const totalStockValue = products.reduce((total, p) => total + (p.price * p.stock_quantity), 0);
-    const lowStockProducts = products.filter(p => p.stock_quantity <= LOW_STOCK_THRESHOLD);
+  const totalStockValue = products.reduce((total, p) => total + p.price * p.stock_quantity, 0);
+  const lowStockProducts = products.filter((p) => p.stock_quantity <= LOW_STOCK_THRESHOLD);
 
-    const optionsBar = {
-        plugins: {
-            legend: {
-                display: true,
-                position: 'top',
-            }
-        },
-        animation: {
-            tension: {
-                duration: 1000,
-                easing: 'easeInOutCubic',
-                from: 1,
-                to: 0,
-                loop: true
-            }
-        },
-        hover: {
-            animationDuration: 1000,
-        },
-        responsiveAnimationDuration: 1000,
-    };
+  const optionsBar = {
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+    animation: {
+      tension: {
+        duration: 1000,
+        easing: 'easeInOutCubic',
+        from: 1,
+        to: 0,
+        loop: true,
+      },
+    },
+    hover: {
+      animationDuration: 1000,
+    },
+    responsiveAnimationDuration: 1000,
+  };
 
-    const optionsPieDoughnut = {
-        plugins: {
-            legend: {
-                display: true,
-                position: 'top',
-            }
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true,
-            duration: 1000,
-        },
-    };
+  const optionsPieDoughnut = {
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+    animation: {
+      animateScale: true,
+      animateRotate: true,
+      duration: 1000,
+    },
+  };
 
-    return (
-        <div className="products-dashboard">
-            <h1>Products Analytics Dashboard</h1>
-
-            <section className="info-cards">
-                <div className="info-card">
-                    <h2>Total Products</h2>
-                    <p>{products.length}</p>
-                </div>
-                <div className="info-card">
-                    <h2>Total Stock Value</h2>
-                    <p>${totalStockValue.toFixed(2)}</p>
-                </div>
-            </section>
-            <div className="chart-container" style={{height: '40vh', display: "flex", flexDirection: "column", alignItems: "center"}}>
-                <h2>Product Stock Quantities</h2>
-                <Bar data={dataBar} options={optionsBar} />
-            </div>
-            <section className="charts">
-                <div className="chart-container">
-                    <h2>Stock Distribution Across Products</h2>
-                    <Pie data={dataPie} options={optionsPieDoughnut} />
-                </div>
-                <div className="chart-container">
-                    <h2>Revenue Potential Per Product</h2>
-                    <Doughnut data={dataDoughnut} options={optionsPieDoughnut} />
-                </div>
-            </section>
-
-            <section className="low-stock">
-                <h2>Low Stock Alerts</h2>
-                <ul>
-                    {lowStockProducts.map(p => (
-                        <li key={p.product_id}>{p.product_name} - Only {p.stock_quantity} left in stock!</li>
-                    ))}
-                </ul>
-            </section>
+  return (
+    <div className="products-dashboard">
+      <h1>Products Analytics Dashboard</h1>
+      {lowStockProducts.length > 0 && (
+        <section className="low-stock">
+          <h2>Low Stock Alerts</h2>
+          <ul>
+            {lowStockProducts.map((p) => (
+              <li key={p.product_id}>
+                {p.product_name} - Only {p.stock_quantity} left in stock!
+              </li>
+            ))}
+          </ul>
+          <button onClick={handleReorderStockClick}>Reorder All Low Stock</button>
+        </section>
+      )}
+      <section className="info-cards">
+        <div className="info-card">
+          <h2>Total Products</h2>
+          <p>{products.length}</p>
         </div>
-    );
-}
+        <div className="info-card">
+          <h2>Total Stock Value</h2>
+          <p>${totalStockValue.toFixed(2)}</p>
+        </div>
+      </section>
+      <div
+        className="chart-container"
+        style={{ height: '40vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      >
+        <h2>Product Stock Quantities</h2>
+        <Bar data={dataBar} options={optionsBar} />
+      </div>
+      <section className="charts">
+        <div className="chart-container">
+          <h2>Stock Distribution Across Products</h2>
+          <Pie data={dataPie} options={optionsPieDoughnut} />
+        </div>
+        <div className="chart-container">
+          <h2>Revenue Potential Per Product</h2>
+          <Doughnut data={dataDoughnut} options={optionsPieDoughnut} />
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default ProductsDashboard;
